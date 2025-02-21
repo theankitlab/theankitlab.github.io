c76bfb7e3be5be5362380954bbb4f33c90b480e1
Ankit kumar <iamankitm05@gmail.com>
2025-02-21 14:46:31 +0530

    Added try catch in fetch project

--- index.js
+++ index.js
@@ -13,23 +13,38 @@ window.addEventListener("resize", function () {
 });
 
 
-window.onload = async () => {
+window.onload = fetchProjects;
+
+async function fetchProjects() {
   const projectsContainer = document.getElementById('projectsContainer');
   projectsContainer.innerHTML = '<div class="loader"></div>';
 
-  const response = await fetch('https://api.github.com/users/theankitlab/repos');
-  const repos = await response.json();
+  try {
+    const [response1, response2] = await Promise.all([
+      fetch('projects.json'),
+      fetch('https://api.github.com/users/theankitlab/repos'),
+    ])
 
-  projectsContainer.innerHTML = "";
+    const [myProjects, githubProjects] = await Promise.all([
+      response1.json(),
+      response2.json(),
+    ])
 
-  repos.forEach(repo => {
+    const repos = [
+      ...myProjects,
+      ...githubProjects
+    ];
 
-    // repo properties:-
-    // name, html_url, created_at, updated_at, 
-    // pushed_at, language, forks, open_issues, 
-    // watchers, watchers_count
+    projectsContainer.innerHTML = "";
 
-    projectsContainer.innerHTML += `
+    repos.forEach(repo => {
+
+      // repo properties:-
+      // name, html_url, created_at, updated_at, 
+      // pushed_at, language, forks, open_issues, 
+      // watchers, watchers_count
+
+      projectsContainer.innerHTML += `
       <a href="${repo.html_url}" target="_blank" class="project">
         <h2>${repo.name}</h2>
         <p>Created At: ${repo.created_at}</p>
@@ -41,10 +56,19 @@ window.onload = async () => {
           </div>
           <div>
             <img src="./public/code-branch.svg" alt="">
-            <p>${repo.forks} forks</p>
+            <p>${repo.forks} Forks</p>
           </div>
         </div>
       </a>
     `;
-  });
+    });
+  } catch (e) {
+    projectsContainer.innerHTML = `
+      <div class="error" onclick="fetchProjects()">
+        <h3>Oops!, something went wrong</h3>
+        <p>${e.message}</p>
+        <div>Try Again</div>
+      </div>
+    `
+  }
 }
